ad369691efc9763b831fcb07edc94914a71d2ee0
kisasexypantera94 <green.grinya@gmail.com>
2018-07-17 01:29:42 +0300

    Done assignment0

--- phase3/blinky.c
+++ phase3/blinky.c
@@ -4,17 +4,30 @@ volatile unsigned *GPIO_FSEL1 = (volatile unsigned *)(GPIO_BASE + 0x04);
 volatile unsigned *GPIO_SET0  = (volatile unsigned *)(GPIO_BASE + 0x1C);
 volatile unsigned *GPIO_CLR0  = (volatile unsigned *)(GPIO_BASE + 0x28);
 
-static void spin_sleep_us(unsigned int us) {
-  for (unsigned int i = 0; i < us * 6; i++) {
-    asm volatile("nop");
-  }
+static void spin_sleep_us(unsigned int us)
+{
+    for (unsigned int i = 0; i < us * 6; i++)
+    {
+        asm volatile("nop");
+    }
 }
 
-static void spin_sleep_ms(unsigned int ms) {
-  spin_sleep_us(ms * 1000);
+static void spin_sleep_ms(unsigned int ms)
+{
+    spin_sleep_us(ms * 1000);
 }
 
-int main(void) {
-  // STEP 1: Set GPIO Pin 16 as output.
-  // STEP 2: Continuously set and clear GPIO 16.
+int main(void)
+{
+    // STEP 1: Set GPIO Pin 16 as output.
+    *GPIO_FSEL1 |= 0x01 << 18;
+
+    // STEP 2: Continuously set and clear GPIO 16.
+    while (1)
+    {
+        *GPIO_SET0 |= 0x01 << 16;
+        spin_sleep_ms(500);
+        *GPIO_CLR0 |= 0x01 << 16;
+        spin_sleep_ms(500);
+    }
 }
